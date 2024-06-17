fedac17a67eeffcd69c11ac3561db2f3d5e90406
Murad <yusifovmurad1@gmail.com>
2024-06-18 02:31:27 +0400

    Update page.tsx

--- app/home-page/job-board/[jobId]/page.tsx
+++ app/home-page/job-board/[jobId]/page.tsx
@@ -95,87 +95,118 @@ const JobDetails: FC<{ params: { jobId: string } }> = ({ params }) => {
       {error}
     </h1>
   ) : (
-    <div className="job-details p-4 flex flex-col justify-center items-center text-whitish">
-      <div className="job-details__logo mb-3">
-        <Image
-          className=""
-          alt="company logo"
-          width={80}
-          objectFit="contain"
-          height={80}
-          src={companyLogo}
-        />
-      </div>
-      <div className="job-details__body flex flex-col justify-center items-center gap-1">
-        <h3 className="job-details__title text-center text-xl font-semibold">
-          {jobDetails?.job_title || "Job title"}
-        </h3>
-        <div className="job-sub-details flex flex-col items-center justify-center">
-          <a
-            target="_blank"
-            href={jobDetails?.employer_website || ""}
-            className="job-details__company flex items-center gap-1"
-          >
-            {jobDetails?.employer_name || "Company name"}{" "}
-            <LinkIconCompany className="size-3 fill-whitish" />
-          </a>
-          {companyLocation && (
-            <div className="job-details__location text-center">
-              {companyLocation}
-            </div>
-          )}
-          {companySalary && (
-            <div className="job-details__salary text-center">
-              {companySalary}
-            </div>
-          )}
-          {jobDetails?.job_employment_type && (
-            <p className="text-center text-sm text-whitish font-semibold">
-              {jobDetails?.job_employment_type}
+    <>
+      <div className="job-details p-4  sm:px-[25px] md:px-[50px] lg:hidden flex flex-col justify-center items-center text-whitish">
+        <div className="job-details__logo mb-3">
+          <Image
+            className=""
+            alt="company logo"
+            width={80}
+            objectFit="contain"
+            height={80}
+            src={companyLogo}
+          />
+        </div>
+        <div className="job-details__body flex flex-col justify-center items-center gap-1">
+          <h3 className="job-details__title text-center text-xl font-semibold">
+            {jobDetails?.job_title || "Job title"}
+          </h3>
+          <div className="job-sub-details flex flex-col items-center justify-center">
+            <a
+              target="_blank"
+              href={jobDetails?.employer_website || ""}
+              className="job-details__company flex items-center gap-1"
+            >
+              {jobDetails?.employer_name || "Company name"}{" "}
+              <LinkIconCompany className="size-3 fill-whitish" />
+            </a>
+            {companyLocation && (
+              <div className="job-details__location text-center">
+                {companyLocation}
+              </div>
+            )}
+            {companySalary && (
+              <div className="job-details__salary text-center">
+                {companySalary}
+              </div>
+            )}
+            {jobDetails?.job_employment_type && (
+              <p className="text-center text-sm text-whitish font-semibold">
+                {jobDetails?.job_employment_type}
+              </p>
+            )}
+          </div>
+          <button className="apply-button flex items-center justify-center">
+            <a
+              target="_blank"
+              className="p-2 bg-secondary rounded-lg"
+              href={jobDetails?.job_apply_link || ""}
+            >
+              Apply now
+            </a>
+          </button>
+        </div>
+        <div className="job-more-details w-full mt-4">
+          <p className="text-whitish font-semibold text-[20px]">
+            About the job
+          </p>
+          {jobDetails?.job_description && (
+            <p className="text-whitish whitespace-pre-line mt-2 text-sm">
+              {showTrimmedText ? trimmedText : text}
             </p>
           )}
         </div>
-        <button className="apply-button flex items-center justify-center">
-          <a
-            target="_blank"
-            className="p-2 bg-secondary rounded-lg"
-            href={jobDetails?.job_apply_link || ""}
+        {showTrimmedText && (
+          <motion.button
+            variants={BUTTON_VARIANTS}
+            whileHover="whileHover"
+            whileTap="whileTap"
+            className="mt-4 p-2 rounded-lg fixed bottom-5 text-sm sm:text-base font-semibold  bg-primary/20 backdrop-blur-lg border border-secondary/80"
+            onClick={() => setShowTrimmedText(false)}
           >
-            Apply now
-          </a>
-        </button>
-      </div>
-      <div className="job-more-details w-full mt-4">
-        <p className="text-whitish font-semibold text-[20px]">About the job</p>
-        {jobDetails?.job_description && (
-          <p className="text-whitish whitespace-pre-line mt-2 text-sm">
-            {showTrimmedText ? trimmedText : text}
-          </p>
+            See more
+          </motion.button>
+        )}
+        {!showTrimmedText && (
+          <motion.button
+            variants={BUTTON_VARIANTS}
+            whileHover="whileHover"
+            whileTap="whileTap"
+            className="mt-4 p-2 rounded-lg fixed bottom-5 text-sm sm:text-base font-semibold bg-primary/20 backdrop-blur-lg border border-secondary/80"
+            onClick={() => setShowTrimmedText(true)}
+          >
+            See less
+          </motion.button>
         )}
       </div>
-      {showTrimmedText && (
-        <motion.button
-          variants={BUTTON_VARIANTS}
-          whileHover="whileHover"
-          whileTap="whileTap"
-          className="mt-4 p-2 rounded-lg fixed bottom-5 text-sm sm:text-base font-semibold  bg-primary/20 backdrop-blur-lg border border-secondary/80"
-          onClick={() => setShowTrimmedText(false)}
-        >
-          See more
-        </motion.button>
-      )}
-      {!showTrimmedText && (
-        <motion.button
-          variants={BUTTON_VARIANTS}
-          whileHover="whileHover"
-          whileTap="whileTap"
-          className="mt-4 p-2 rounded-lg fixed bottom-5 text-sm sm:text-base font-semibold bg-primary/20 backdrop-blur-lg border border-secondary/80"
-          onClick={() => setShowTrimmedText(true)}
-        >
-          See less
-        </motion.button>
-      )}
-    </div>
+      <div className="job-details-desktop hidden p-4  lg:grid grid-cols-12 text-whitish">
+        <div className="job-details__left col-span-8 rounded-lg bg-whitish text-primary p-4">
+          <div className="job-details__top">
+            <h2 className="job-details__title font-semibold text-2xl">
+              {jobDetails?.job_title || "Job title"}
+            </h2>
+            {companyLocation && (
+              <p className="job-details__location">{companyLocation}</p>
+            )}
+            {companySalary && (
+              <p className="job-details__salary">{companySalary}</p>
+            )}
+            {jobDetails?.job_employment_type && (
+              <p className="job-details__employment-type text-sm">
+                {jobDetails?.job_employment_type}
+              </p>
+            )}
+          </div>
+          <div className="job-details__body mt-4">
+            <p className="job-details__about text-xl font-medium">About job</p>
+            {jobDetails?.job_description && (
+              <p className="text-sm mt-2 whitespace-pre-line">{text}</p>
+            )}
+          </div>
+        </div>
+        <div className="job-details__right"></div>
+      </div>
+    </>
   );
 };
 
